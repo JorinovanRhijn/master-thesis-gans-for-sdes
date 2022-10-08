a2fd9ec6d8533a03c00a8c46d8f08410660944fc
Jorino <jorino@agga.capital>
2022-10-08 20:50:53 +0200

    added preset type to presets

--- presets.py
+++ presets.py
@@ -1,8 +1,9 @@
 import numpy as np
 from sde_dataset import SDEDataset
+from data_types import Preset
 
 
-def load_preset(case, N_train=10_000, N_test=10_000):
+def load_preset(case: Preset, N_train: int = 10_000, N_test: int = 10_000):
     '''
     Method to construct a preset to easily start one of the different cases
     '''
@@ -18,41 +19,36 @@ def load_preset(case, N_train=10_000, N_test=10_000):
     data.params['t'] = 1
 
     # Four pre-set cases
-    cases = ['GBM', 'CIR_Feller_satisfied', 'CIR_Feller_violated_moderate_gamma', 'CIR_Feller_violated_high_gamma']
 
-    if case == cases[0]:
-        # GBM
+    if case is Preset.GBM:
         data.params['mu'] = 0.05
         data.params['sigma'] = 0.2
         data.params['S0'] = 1
-    elif case == cases[1]:
-        # CIR Feller satisfied
+    elif case is Preset.CIR_FELLER_SATISFIED:
         data.params['gamma'] = 0.1
         data.params['kappa'] = 0.1
         data.params['S_bar'] = 0.1
         data.params['S0'] = 0.1
         S_vec = np.linspace(0.01, 0.5, 20)
-    elif case == cases[2]:
-        # CIR Feller violated,  gamma=0.3
+    elif case is Preset.CIR_FELLER_VIOLATED_LOW_GAMMA:
         data.params['gamma'] = 0.3
         data.params['kappa'] = 0.1
         data.params['S_bar'] = 0.1
         data.params['S0'] = 0.1
         S_vec = np.array([1e-4, 5e-4, 1e-3, 5e-3, 0.01, 0.02, 0.03, 0.04,
                           0.05, 0.07, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-    elif case == cases[3]:
-        # CIR Feller violated,  gamma=0.6
+    elif case is Preset.CIR_FELLER_VIOLATED_HIGH_GAMMA:
         data.params['gamma'] = 0.6
         data.params['kappa'] = 0.1
         data.params['S_bar'] = 0.1
         data.params['S0'] = 0.1
         S_vec = np.concatenate((np.logspace(-6, 0, 12), np.array([0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 1.5])))
     else:
-        raise ValueError('Case {0} not specified in presets. Options are {1}'.format(case, str(cases)))
+        raise ValueError('Case {0} not specified in presets. Options are {1}'.format(case, str(Preset._member_names_)))
 
     # Set up the condition vectors: for the CIR process,
     # a random sample is taken from the Cartesian product of the two conditions
-    if case == cases[0]:
+    if case is Preset.GBM:
         # GBM
         data.C = dict(t=ts.repeat(data.N//len(ts))[np.random.permutation(data.N)])
         data.C_test = dict(t=1)
