fdfa01e37ab0b7df1246b7cd28a6117c84109b22
Jorino <jorino@agga.capital>
2022-10-15 14:02:13 +0200

    pep8 train_step

--- train_step.py
+++ train_step.py
@@ -125,79 +125,101 @@ def vanilla_CGAN_step(netD,
     return errD, errG, D_x, D_G_z1, D_G_z2
 
 
-def supervised_GAN_step(netD,netG,optD,optG,loss_func,device,X_i=None,C_i=None,Z_i=None,real_label=1,fake_label=0,n_D=1):
-    # Train discriminator n_D times per batch 
+def supervised_GAN_step(netD,
+                        netG,
+                        optD,
+                        optG,
+                        loss_func,
+                        device,
+                        X_i=None,
+                        C_i=None,
+                        Z_i=None,
+                        real_label=1,
+                        fake_label=0,
+                        n_D=1):
+    # Train discriminator n_D times per batch
     for j in range(n_D):
-        # Train on all-real batch 
+        # Train on all-real batch
         netD.zero_grad()
-        real_sample = torch.cat((X_i,Z_i),axis=1)
+        real_sample = torch.cat((X_i, Z_i), axis=1)
         b_size = X_i.size(0)
-        label = torch.full((b_size,), real_label, device=device,dtype=torch.get_default_dtype())
+        label = torch.full((b_size,), real_label, device=device, dtype=torch.get_default_dtype())
         output = netD(real_sample)
-        errD_real = loss_func(output.view(-1),label.view(-1))
+        errD_real = loss_func(output.view(-1), label.view(-1))
         errD_real.backward()
         D_x = output.mean().item()
 
-        # Train on all-fake batch 
-        input_G = input_sample(b_size,Z=Z_i,device=device)
-        fake = torch.cat((netG(input_G).detach(),Z_i),axis=1)
+        # Train on all-fake batch
+        input_G = input_sample(b_size, Z=Z_i, device=device)
+        fake = torch.cat((netG(input_G).detach(), Z_i), axis=1)
         label.fill_(fake_label)
         output = netD(fake)
-        errD_fake = loss_func(output.view(-1),label.view(-1))
+        errD_fake = loss_func(output.view(-1), label.view(-1))
         errD_fake.backward()
         D_G_z1 = output.mean().item()
         errD = errD_real + errD_fake
         optD.step()
 
-    # Train generator 
+    # Train generator
     netG.zero_grad()
 
-    # Create fake sample 
+    # Create fake sample
     # input_G = input_sample(b_size,Z=Z_i,device=device)
-    fake = torch.cat((netG(input_G),Z_i),axis=1)
+    fake = torch.cat((netG(input_G), Z_i), axis=1)
     output = netD(fake)
-    errG = -torch.log(output).mean() # 'Logtrick'
+    errG = -torch.log(output).mean()  # 'Logtrick'
     errG.backward()
     D_G_z2 = output.mean().item()
     optG.step()
 
-    return errD,errG,D_x,D_G_z1,D_G_z2
+    return errD, errG, D_x, D_G_z1, D_G_z2
 
 
-def supervised_CGAN_step(netD,netG,optD,optG,loss_func,device,X_i=None,C_i=None,Z_i=None,real_label=1,fake_label=0,n_D=1):
-    # Train discriminator n_D times per batch 
+def supervised_CGAN_step(netD,
+                         netG,
+                         optD,
+                         optG,
+                         loss_func,
+                         device,
+                         X_i=None,
+                         C_i=None,
+                         Z_i=None,
+                         real_label=1,
+                         fake_label=0,
+                         n_D=1):
+    # Train discriminator n_D times per batch
     for j in range(n_D):
-        # Train on all-real batch 
+        # Train on all-real batch
         netD.zero_grad()
-        real_sample = torch.cat((X_i,Z_i,C_i),axis=1)
+        real_sample = torch.cat((X_i, Z_i, C_i), axis=1)
         b_size = X_i.size(0)
-        label = torch.full((b_size,), real_label, device=device,dtype=torch.get_default_dtype())
+        label = torch.full((b_size,), real_label, device=device, dtype=torch.get_default_dtype())
         output = netD(real_sample)
-        errD_real = loss_func(output.view(-1),label.view(-1))
+        errD_real = loss_func(output.view(-1), label.view(-1))
         errD_real.backward()
         D_x = output.mean().item()
 
-        # Train on all-fake batch 
-        input_G = torch.cat((Z_i,C_i),axis=1)
-        fake = torch.cat((netG(input_G).detach(),Z_i,C_i),axis=1)
+        # Train on all-fake batch
+        input_G = torch.cat((Z_i, C_i), axis=1)
+        fake = torch.cat((netG(input_G).detach(), Z_i, C_i), axis=1)
         label.fill_(fake_label)
         output = netD(fake)
-        errD_fake = loss_func(output.view(-1),label.view(-1))
+        errD_fake = loss_func(output.view(-1), label.view(-1))
         errD_fake.backward()
         D_G_z1 = output.mean().item()
         errD = errD_real + errD_fake
         optD.step()
 
-    # Train generator 
+    # Train generator
     netG.zero_grad()
 
-    # Create fake sample 
+    # Create fake sample
     # input_G = input_sample(b_size,Z=Z_i,C=C_i,device=device)
-    fake = torch.cat((netG(input_G),Z_i,C_i),axis=1)
+    fake = torch.cat((netG(input_G), Z_i, C_i), axis=1)
     output = netD(fake)
-    errG = -torch.log(output).mean() # 'Logtrick'
+    errG = -torch.log(output).mean()  # 'Logtrick'
     errG.backward()
     D_G_z2 = output.mean().item()
     optG.step()
 
-    return errD,errG,D_x,D_G_z1,D_G_z2
+    return errD, errG, D_x, D_G_z1, D_G_z2
